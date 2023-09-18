db98c38d129b44eb2e59bf93408ef53b860185c1
asaddevvv <nabievasad17@gmail.com>
2023-09-18 14:54:02 +0000

    domdz

---
+++ main.js
@@ -0,0 +1,94 @@
+const block_bigg = document.querySelector(".block_bigg");
+// const block1 = document.querySelector(".block_1");
+// const block2 = document.querySelector(".block_2");
+// const block3 = document.querySelector(".block_3");
+// const block4 = document.querySelector(".block_4");
+// const block5 = document.querySelector(".block_5");
+// const block6 = document.querySelector(".block_6");
+// const block7 = document.querySelector(".block_7");
+// const block8 = document.querySelector(".block_8");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2")
+const btn3 = document.querySelector(".btn3")
+const btn4 = document.querySelector(".btn4")
+const btn5 = document.querySelector(".btn5")
+const btn6 = document.querySelector(".btn6")
+const btn7 = document.querySelector(".btn7")
+const btn8 = document.querySelector(".btn8")
+const btn9 = document.querySelector(".btn9")
+const btn10 = document.querySelector(".btn10")
+const btn11 = document.querySelector(".btn11")
+const btn12 = document.querySelector(".btn12")
+const btn13 = document.querySelector(".btn13")
+const btn14 = document.querySelector(".btn14")
+const btn15 = document.querySelector(".btn15")
+const man = document.querySelector(".man");
+const block_btn = document.querySelector(".block_btn");
+
+btn1.addEventListener("click", () => {
+        man.style.marginTop = "440px";
+        man.style.transition = ".8s"
+    
+});
+btn2.addEventListener("click", () => {
+    man.style.marginTop = "410px";
+    man.style.transition = ".8s"
+  });
+btn3.addEventListener("click", () => {
+    man.style.marginTop = "380px";
+    man.style.transition = ".8s"
+
+});
+btn4.addEventListener("click", () => {
+man.style.marginTop = "345px";
+man.style.transition = ".8s"
+});
+btn5.addEventListener("click", () => {
+    man.style.marginTop = "315px";
+    man.style.transition = ".8s"
+
+});
+btn6.addEventListener("click", () => {
+man.style.marginTop = "285px";
+man.style.transition = ".8s"
+});
+btn7.addEventListener("click", () => {
+    man.style.marginTop = "255px";
+    man.style.transition = ".8s"
+
+});
+btn8.addEventListener("click", () => {
+man.style.marginTop = "215px";
+man.style.transition = ".8s"
+});
+btn9.addEventListener("click", () => {
+    man.style.marginTop = "185px";
+    man.style.transition = ".8s"
+
+});
+btn10.addEventListener("click", () => {
+man.style.marginTop = "160px";
+man.style.transition = ".8s"
+});
+btn11.addEventListener("click", () => {
+    man.style.marginTop = "125px";
+    man.style.transition = ".8s"
+
+});
+btn12.addEventListener("click", () => {
+man.style.marginTop = "95px";
+man.style.transition = ".8s"
+});
+btn13.addEventListener("click", () => {
+    man.style.marginTop = "65px";
+    man.style.transition = ".8s"
+
+});
+btn14.addEventListener("click", () => {
+man.style.marginTop = "35px";
+man.style.transition = ".8s"
+});
+btn15.addEventListener("click", () => {
+    man.style.marginTop = "5px";
+    man.style.transition = ".8s"
+    });
